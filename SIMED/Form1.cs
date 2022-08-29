619b39dea76fab10711e5d6cef7db02050aa6d7b
juanma193 <66088806+juanma193@users.noreply.github.com>
2022-08-29 13:44:38 -0300

    Conexion con BD

--- SIMED/Form1.cs
+++ SIMED/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,18 @@ namespace SIMED
 {
     public partial class Form1 : Form
     {
+        SqlConnection sql;
+        string connetionString = @"Data Source=200.69.137.167,11333;Initial Catalog=BD3K3G05_2022;User ID=BD3K3G05_2022;Password=CLV05_25089";
         public Form1()
         {
             InitializeComponent();
+            sql = new SqlConnection(connetionString);
+            sql.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
